f4cbcf038b74e925bf36b2f4f96b78a5a8d6f829
x00111894 <x00111894@gmail.com>
2014-10-16 10:42:32 +0100

    X
    C X X
    X
    X

---
+++ x00111894-grades.sh
@@ -0,0 +1,35 @@
+#Test case 1 
+#Input : 50 70 
+#Expected Output : Pass
+#Observed Output : Fail
+java grades 50 70
+
+#Test case 2  
+#Input : 30 90
+#Expected Output : Component fail 
+#Observed Output : Component fail
+java grades 30 90
+
+#Test case 3 
+#Input : 100 100 
+#Expected Output : Pass with distinction 
+#Observed Output : Pass with distinction 
+java grades 100 100
+
+#Test case 4 
+#Input : 80 40
+#Expected Output : Pass  
+#Observed Output : Pass
+java grades 80 40
+
+#Test case 5 
+#Input : 110 50
+#Expected Output : Invalid input  
+#Observed Output : Invalid input 
+java grades 110 50
+
+#Test case 6 
+#Input : 60 41
+#Expected Output : Pass
+#Observed Output : Fail
+java grades 60 41
